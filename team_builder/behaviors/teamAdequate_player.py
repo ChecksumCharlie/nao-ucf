9f5a6214b484cd254777b7a9801fec435a893e31
ChecksumCharlie <nottheaddressyourelookingfor@server.fake>
2014-04-16 22:52:31 -0400

    More finishing stuff

--- team_builder/behaviors/teamAdequate_player.py
+++ team_builder/behaviors/teamAdequate_player.py
@@ -15,10 +15,13 @@ class LogicFor:
         param.wrapFSM(self)
 
     def freePath(self, enemy_coords):
+        if (enemy_coords == []):
+            return False
+
         num_enemy = int(math.floor(len(enemy_coords)/2))
 
         for x in xrange(num_enemy):
-            if (enemy_coords[2*x] > 250 and enemy_coords[2*x] < 390):
+            if (enemy_coords[2*x] > 290 and enemy_coords[2*x] < 360):
                 return True
 
         return False
@@ -39,14 +42,15 @@ class LogicFor:
 
         if (self.player.hasFallen()):
             self.player.initStance()
+            self.player.setHeadAngle(0, "HeadPitch")
 
         if (RedBall == [] and Goal != [] and RedBall_lower != []):
             print "Positioning"
             
             if (Goal[0] < 305):#shift and roate right
-                self.player.walk2(0.0, 0.1, 0.2)
+                self.player.walk2(0.0, 0.1, 0.1)
             elif (Goal[0] > 335):#Shift and rotate left
-                self.player.walk2(0.0, -0.1, -0.2)
+                self.player.walk2(0.0, -0.1, -0.1)
             else:
                 pass
 
@@ -54,12 +58,37 @@ class LogicFor:
                 if(self.freePath(Enemy)):#path blocked
                     print "Path Blocked."
                     self.player.stop()
-                    if (RedBall_lower[0]<300):
-                        self.player.sidestep(0.2)
-                    elif (RedBall_lower[0]>340):
-                        self.player.sidestep(-0.2)
-                    else:
-                        self.player.stop()
+                    
+                    while (True):
+                        RedBall_lower = self.player.getRedBall2()
+                        if (RedBall_lower == []):
+                            break
+                        if (self.freePath(Enemy)):#shift and roate right
+                            self.player.walk2(0.0, 0.1, 0.1)
+                        else:
+                            pass
+
+
+                    if(RedBall_lower != []):
+                        while(True):
+                                RedBall_lower = self.player.getRedBall2()
+                                print "In position Loop (Blocked)"
+                                if(RedBall_lower[1] >=470 or RedBall_lower == []):
+                                    self.player.killWalk()
+                                    break
+                                if (RedBall_lower[0] == 305):
+                                    theta = 0.0
+                                elif (RedBall_lower[0] < 305):
+                                    theta = math.atan((475 - RedBall_lower[1])/(305 - RedBall_lower[0])) - math.radians(90)
+                                else:
+                                    theta = math.fabs(math.atan((475 - RedBall_lower[1])/(RedBall_lower[0] - 305))  - math.radians(90))
+                                self.player.walk2(0.35, 0.0, 0.2*-theta)
+                                time.sleep(0.1)
+                        print "KICKING"
+
+                        self.player.leftKick(1.0,1.1,2.3)
+                        time.sleep(1.0)
+
 
                 else:#free path
                     print "Free Path."
@@ -67,24 +96,25 @@ class LogicFor:
                         while(True):
                             RedBall_lower = self.player.getRedBall2()
                             print "In position Loop"
-                            if(RedBall_lower[1] >=470 or RedBall_lower == []):
-                                if(RedBall_lower[0] > 310):
-                                    self.player.sidestep(0.2)
-                                    time.sleep(1)
+                            if(RedBall_lower[1] >=470 or RedBall_lower == [] or self.freePath(self.player.getPinkPlayers())):
                                 self.player.killWalk()
                                 break
-                            if (RedBall_lower[0] == 320):
+                            if (RedBall_lower[0] == 305):
                                 theta = 0.0
-                            elif (RedBall_lower[0] < 320):
-                                theta = math.atan((475 - RedBall_lower[1])/(320 - RedBall_lower[0])) - math.radians(90)
+                            elif (RedBall_lower[0] < 305):
+                                theta = math.atan((475 - RedBall_lower[1])/(305 - RedBall_lower[0])) - math.radians(90)
                             else:
-                                theta = math.fabs(math.atan((475 - RedBall_lower[1])/(RedBall_lower[0] - 320))  - math.radians(90))
+                                theta = math.fabs(math.atan((475 - RedBall_lower[1])/(RedBall_lower[0] - 305))  - math.radians(90))
                             self.player.walk2(0.35, 0.0, 0.2*-theta)
                             time.sleep(0.1)
-                        print "KICKING"
 
-                        self.player.leftKick(1.0,1.1,2.3)
-                        time.sleep(1.0)            
+                        if (self.freePath(self.player.getPinkPlayers)):
+                            print "No shot to kick"
+                        else:
+                            print "KICKING"
+
+                            self.player.leftKick(1.0,1.1,2.3)
+                            time.sleep(1.0)            
                     else: 
                         self.player.stop()
         elif (RedBall == [] and RedBall_lower == [] ):#ball not in view
